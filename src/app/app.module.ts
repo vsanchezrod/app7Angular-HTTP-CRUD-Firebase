5b1a844ef0525cf2c16a7e5a5f3de85651495d40
vsanchezrod <v.sanchez.rod@gmail.com>
2018-08-03 10:33:57 +0200

    Componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Para trabajar con formularios
+import { FormsModule } from '@angular/forms';
+
+// Para trabajar con HTTP
+import { HttpModule } from '@angular/http';
+
+
+// Componentes
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
